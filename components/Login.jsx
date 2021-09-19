4c58709c3081227cb99c7e2f67bc3c972d0395ce
Rohit <rohitp934@gmail.com>
2021-09-19 12:53:02 +0530

    Implementing Auth

---
+++ components/Login.jsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import { signIn } from 'next-auth/client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
+const Login = () => {
+	return (
+		<div className='flex flex-col items-center'>
+			<Image
+				src='https://links.papareact.com/t4i'
+				height={400}
+				width={400}
+				objectFit='contain'
+				alt='Login'
+			/>
+			<h1 className='text-3xl font-semibold'>Login to Facebook</h1>
+			<div
+				className='p-5 m-5 mt-20 flex items-center  bg-gray-100
+				rounded-3xl hover:bg-black transition duration-300 
+				cursor-pointer group'
+				onClick={() => {
+					signIn('github')
+				}}
+			>
+				<FontAwesomeIcon
+					size='3x'
+					icon={faGithub}
+					className='group-hover:hidden'
+				/>
+				<FontAwesomeIcon
+					inverse
+					size='3x'
+					icon={faGithub}
+					className='hidden group-hover:block'
+				/>
+				<p className='text-xl pl-6 group-hover:text-white'>
+					Sign in with Github
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default Login
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,29 @@
+import { getSession, useSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
-
+import Login from '../components/Login'
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Facebook</title>
-        <meta name="description" content="Facebook Clone by Rohit Prakash" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
+	const [session, loading] = useSession()
+	if (!session) return <Login />
+	return (
+		<div>
+			<Head>
+				<title>Facebook</title>
+				<meta name='description' content='Facebook Clone by Rohit Prakash' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Header />
+		</div>
+	)
+}
 
-    </div>
-  )
+export const getServerSideProps = async (context) => {
+	//Get user props;
+	const session = await getSession(context)
+
+	return {
+		props: {
+			session,
+		},
+	}
 }
